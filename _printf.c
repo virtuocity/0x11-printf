3ec843b10cf59b427f72bb526d26527b798aec66
gabaya <gabayaconsult@gmail.com>
2022-10-20 19:49:32 +0000

    partial _printf

--- _printf.c
+++ _printf.c
@@ -8,10 +8,23 @@
   */
 int _printf(const char *format, ...)
 {
+	char *s;
+	char *c;
 	unsigned int i;
-
-	for (i = 0;*(s + i) != '\0';i++)
-		write(1,&s[i],1);
+	
+	if (format == NULL)
+		return (1);
+	s = (char *)malloc(sizeof(format));
+	i = 0;
+	va_list valist;
+	va_start(valist,format);
+	for (s = format; *s != '\0'; s++)
+	{
+		while (*(s + i) != '%')
+		{
+			write(1,&s[i],1);
+			i++;
+		}
 	
 	write(1,"\n", 1);
 }
